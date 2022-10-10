1affe82ba4c5fb582d8fcae800853e891b70e1d6
Vy <tuongvy.tran@gmail.com>
2022-10-09 23:40:06 -0500

    jspart1

---
+++ dom.js
@@ -0,0 +1,34 @@
+// EXAMINE THE DOCUMNET OBJECT
+
+// console.dir(document); show all properties of document
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = "Hello"; 
+// text.Content remove and replace the text content of the specific note and all its decendants by the new text
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// GETELEMENTBYID
+console.log(document.getElementById("header-title"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
